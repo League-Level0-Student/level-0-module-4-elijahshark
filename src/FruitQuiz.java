50889dd523ac8cf42e06a264ff20567999f53ace
League Student <league@iMac-4.attlocal.net>
2018-11-07 16:58:15 -0800

    FuitQuiz(IP)

--- src/FruitQuiz.java
+++ src/FruitQuiz.java
@@ -15,6 +15,14 @@ public class FruitQuiz extends KeyAdapter {
 	void makeQuestions() {
 		question1 = new JLabel(
 				"<html>Which is not a real fruit? <br> A: Dragon Fruit <br> B: Durian <br> C: Crazyberry</html>");
+
+		question2 = new JLabel(
+				"<html>what is the two colors of the scar? <br> A: Golden,purple <br> B: blue <br> C: green</html>");
+		question3 = new JLabel(
+				"<html>what is the two colors of the scar? <br> A: Golden,purple <br> B: blue <br> C: green</html>");
+		question4 = new JLabel(
+				"<html>what is the two colors of the scar? <br> A: Golden,purple <br> B: blue <br> C: green</html>");
+		
 		// 11. Make another question called "question2". Use question1 above as a guide.
 		// ...
 	}
@@ -34,23 +42,29 @@ public class FruitQuiz extends KeyAdapter {
 		// 12. If question2 is showing,
 
 		// 13. check if it is right or wrong like you did for question1
-
-		if (question1.isShowing()) {
+		if (question2.isShowing()) {
+			if (A == keyCode) {
+				correct();
+				nextQuestion(question3);
+			}
+		}
+			if (question1.isShowing()) {
 			// 3. If they selected the right fruit, do steps 4 and 7
 			if (C == keyCode) {
 				correct();
+				nextQuestion(question2);
 			}
 			// 4. Call the correct() method
-			 
+
 			// 7. Use the nextQuestion() method to go to question2
-			nextQuestion("question2");
+			
 			// 8. else (if they touched something else)
 
 			// 9. Call the incorrect() method
-if (keyCode != C) {
-incorrect();
-}		
-}
+			if (keyCode != C) {
+				incorrect();
+			}
+		}
 
 	}
 
@@ -59,13 +73,13 @@ incorrect();
 		// the default package. It must be a .wav file.
 		// There are lots on freesound.org
 		// 6. Use the playSound method to play your sound
-playSound("323703__reitanna__funny-yay (1).wav");
+		playSound("323703__reitanna__funny-yay (1).wav");
 	}
 
 	private void incorrect() {
 		// 10. Find a sound for wrong answers and put it in the default package. Use the
 		// playSound method to play it.
-playSound("wrong.wav");
+		playSound("Super Mario Bros. - Game Over Sound Effect (1).wav");
 	}
 
 	private void nextQuestion(JLabel newQuestion) {
