97d37eb80502fa2dd8cb2d0a6afa6f7feb26fdc8
League Student <league@iMac-4.attlocal.net>
2018-12-12 16:55:41 -0800

    Test Scores (IP)

--- src/extra/Test_Scores.java
+++ src/extra/Test_Scores.java
@@ -1,5 +1,28 @@
 package extra;
 
-public class Test_Scores {
+import javax.swing.JOptionPane;
 
+public class Test_Scores {
+public static void main(String[] args) {
+	String ans = JOptionPane.showInputDialog("what did you get on your last test");
+	int r2 = Integer.parseInt(ans);
+	if (r2 > 50.0) {
+		JOptionPane.showMessageDialog(null, "YOUR THE WORST PERSON TO TAKE A TEST!");
+	}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 }
