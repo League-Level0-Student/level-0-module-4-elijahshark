8dc3cfa7c8ee484d714b2eac8ea6a47a13fdb986
League Student <league@iMac-4.attlocal.net>
2018-12-19 16:57:34 -0800

    test scores,SimpleCalculator (IP)

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -6,10 +6,11 @@ public class SimpleCalculator {
 	public static void main(String[] args) {
 
 		// 1. Get 2 numbers from the user and convert them to integer.
-
-		// 2. Customize pop-up to support add/subtract/multiply/divide operations.
+String ans = JOptionPane.showInputDialog("Give me two numbers");
+		int r2 = Integer.parseInt(ans);
+// 2. Customize pop-up to support add/subtract/multiply/divide operations.
 		int operation = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Button1", "Button2", "Button3", "Button3" },
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Add", "Subtract", "Multiply", "Divide" },
 				null);
 
 		// 5. Call the methods created in steps 3 and 4 to perform the appropriate operation.
@@ -20,6 +21,18 @@ public class SimpleCalculator {
 	// 3. Create method for addition operation.
 	//    Method should create pop-up that shows equation and solution, eg. 45 + 25 = 70.
 	//    HINT: use 'static void add(int num1, num2) { ... }
+static void add(int num1, int num2){
 
-	// 4. Create similar methods for subtraction, multiplication and division.
+}
+static void add(int num1, int num2){
+
+}
+static void add(int num1, int num2){
+
+}
+static void add(int num1, int num2){
+
+}
+
+// 4. Create similar methods for subtraction, multiplication and division.
 }
--- src/extra/Test_Scores.java
+++ src/extra/Test_Scores.java
@@ -5,24 +5,16 @@ import javax.swing.JOptionPane;
 public class Test_Scores {
 public static void main(String[] args) {
 	String ans = JOptionPane.showInputDialog("what did you get on your last test");
-	int r2 = Integer.parseInt(ans);
-	if (r2 > 50.0) {
+	double r2 = Double.parseDouble(ans);
+	if (r2 < 50.0) {
 		JOptionPane.showMessageDialog(null, "YOUR THE WORST PERSON TO TAKE A TEST!");
 	}
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	if (r2 > 51.0) {
+		JOptionPane.showMessageDialog(null, "Wow you studed hard for that test");
+	}
 }
 }
+
+
+
+
